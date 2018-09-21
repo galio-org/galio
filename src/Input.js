5b20f2312e75c223fd493f5831ab602590745eb1
Alin Gheorghe <gheorghe.palin@gmail.com>
2018-09-21 12:27:01 +0300

    Init Input branch

---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+const Input = props => <TextInput />;
+
+const styles = StyleSheet.create({});
+
+Input.propTypes = {};
+
+export default Input;
