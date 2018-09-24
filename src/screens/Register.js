946aacfe8ee81c2620aeb4185e848d0e7b1ec378
Alin Gheorghe <gheorghe.palin@gmail.com>
2018-09-24 17:20:24 +0300

    Started the register screen

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+// galio components
+import { Typography, Navbar } from '../';
 
-const Register = () => <View />;
+const Register = (props) => (
+  <View style={{ flex: 1 }}>
+    <Navbar buttonFunction={() => props.navigation.openDrawer()} />
+  </View>
+);
 
 export default Register;
