ab0ac6f8cbf269cd8e93cee0a0ae527ce2d21066
Hetmann W. Iohan <contact@blogtools.ro>
2018-09-27 13:34:31 +0300

    Grid: added Block component

---
+++ src/Block.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const BASE_SIZE = 14;
+const COLOR_BACKGROUND = '#FFFFFF';
+const COLOR_DEFAULT = '#808080';
+
+export default class Block extends Component {
+  static defaultProps = {
+    row: false,
+    flex: false,
+    center: false,
+    middle: false,
+    top: false,
+    bottom: false,
+    right: false,
+    left: false,
+    borderless: false,
+    shadow: false,
+    space: false,
+    fluid: false,
+    height: false,
+    width: 'auto',
+    shadowColor: false,
+  }
+
+  render() {
+    const {
+      row,
+      flex,
+      center,
+      middle,
+      top,
+      bottom,
+      right,
+      left,
+      shadow,
+      space,
+      fluid,
+      height,
+      shadowColor,
+      borderless,
+      width,
+      children,
+      style,
+      ...props
+    } = this.props;
+
+    const styleBlock = [
+      styles.block,
+      row && styles.row,
+      flex && { flex: flex === true ? 1 : flex },
+      center && styles.center,
+      middle && styles.middle,
+      top && styles.top,
+      bottom && styles.bottom,
+      right && styles.right,
+      left && styles.left,
+      space && { justifyContent: `space-${space}` },
+      shadow && styles.shadow,
+      fluid && styles.fluid,
+      height && { height },
+      width && { width },
+      shadowColor && { shadowColor },
+      borderless && { borderWidth: 0 },
+      style,
+    ];
+
+    return (
+      <View style={styleBlock} {...props}>
+        {children}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'column',
+    backgroundColor: COLOR_BACKGROUND,
+    borderRadius: BASE_SIZE * 0.4,
+    borderWidth: BASE_SIZE * 0.05,
+    borderColor: COLOR_DEFAULT,
+    paddingHorizontal: BASE_SIZE,
+    color: COLOR_DEFAULT,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  middle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  center: {
+    alignSelf: 'center',
+  },
+  top: {
+    alignSelf: 'flex-start',
+  },
+  bottom: {
+    alignSelf: 'flex-end',
+  },
+  shadow: {
+    shadowColor: COLOR_DEFAULT,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.4,
+    shadowRadius: 8,
+    elevation: 1,
+  },
+  fluid: {
+    width: 'auto',
+  },
+});
