33a3e2f881dd65c71ad095c5c4cbd94685e56410
Hetmann W. Iohan <contact@blogtools.ro>
2018-10-03 23:30:34 +0300

    Feedback: Article with cover image updates

--- src/screens/ArticleCover.js
+++ src/screens/ArticleCover.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {
-  View,
   Image,
   StyleSheet,
   StatusBar,
 } from 'react-native';
 import { LinearGradient } from 'expo';
-// galio components
-import { Block, Text, NavBar } from '..';
-import theme from '../theme';
 
-// space-between view and another view for the back button
+// galio components
+import {
+  Block, Icon, Text, NavBar,
+} from '..';
+
+import theme from '../theme';
 
 const ArticleCover = props => (
   <Block flex>
@@ -19,20 +20,33 @@ const ArticleCover = props => (
       style={styles.backgroundImage}
       source={{ uri: 'https://images.unsplash.com/photo-1537005081207-04f90e3ba640?fit=crop&w=764&q=80' }}
     />
-    <LinearGradient colors={['transparent', theme.COLORS.BLACK]} style={styles.gradient} />
     <Block flex space="between" center style={styles.absolute}>
       <NavBar transparent leftIconColor={theme.COLORS.WHITE} onLeftPress={() => props.navigation.openDrawer()} />
       <Block style={styles.articleSummary}>
-        <Text h4 color={theme.COLORS.WHITE} style={{ marginBottom: 15, fontWeight: '300' }}>
-          {"Why is 'The Thing' always looking at you?"}
-        </Text>
-        <Text color={theme.COLORS.WHITE} style={{ marginBottom: 15, fontWeight: '300' }}>
-          Just small talk from the Fantastic Four.
-        </Text>
-        <Text size={theme.SIZES.FONT * 0.75} color={theme.COLORS.WHITE} style={{ marginBottom: 5, fontWeight: '300' }}>
-          So... Did you ever think about this bus? Like... How could this bus
-          have all these weird colors. This purple is really cute though.
-        </Text>
+        <Block row style={{ marginBottom: theme.SIZES.BASE }}>
+          <Block row middle style={{ marginHorizontal: theme.SIZES.BASE }}>
+            <Icon name="eye" family="MaterialCommunityIcons" color={theme.COLORS.WHITE} size={theme.SIZES.FONT * 0.8} />
+            <Text p color={theme.COLORS.WHITE} style={{ marginLeft: theme.SIZES.BASE * 0.25 }}>25.2k</Text>
+          </Block>
+          <Block row middle>
+            <Icon name="heart-outline" family="MaterialCommunityIcons" color={theme.COLORS.WHITE} size={theme.SIZES.FONT * 0.8} />
+            <Text p color={theme.COLORS.WHITE} style={{ marginLeft: theme.SIZES.BASE * 0.25 }}>936</Text>
+          </Block>
+        </Block>
+
+        <Block>
+          <Text h3 color={theme.COLORS.WHITE} style={{ marginBottom: theme.SIZES.BASE, fontWeight: '400' }}>
+            {"Why is 'The Thing' always looking at you?"}
+          </Text>
+          <Text color={theme.COLORS.WHITE} style={{ marginBottom: theme.SIZES.BASE, fontWeight: '500', lineHeight: theme.SIZES.FONT * 1.3 }}>
+            Just small talk from the Fantastic Four.
+          </Text>
+          <Text color={theme.COLORS.WHITE} style={{ marginBottom: theme.SIZES.BASE / 2, fontWeight: '500', lineHeight: theme.SIZES.FONT * 1.3 }}>
+            So... Did you ever think about this bus? Like... How could this bus
+            have all these weird colors. This purple is really cute though.
+          </Text>
+        </Block>
+        <LinearGradient colors={['transparent', theme.COLORS.BLACK]} style={styles.gradient} />
       </Block>
     </Block>
   </Block>
@@ -55,10 +69,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    height: '90%',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-    borderRadius: 6,
+    height: '30%',
   },
   articleSummary: {
     paddingLeft: 20,
