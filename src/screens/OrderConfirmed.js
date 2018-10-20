c537781e53a9b453a85b210c4a78bfe713c3b82a
Hoa Nguyen <nvh0412@gmail.com>
2018-10-20 22:06:29 +0700

    Screens/order confirmed (#35)
    * Initialize OrderConfirmed Screen
    
    * Finish screen
    
    * Address comments

---
+++ src/screens/OrderConfirmed.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Platform,
+  Image,
+  Link
+} from 'react-native';
+
+import theme from '../theme';
+
+// galio components
+import {
+  Text, Button, Block, NavBar,
+} from 'galio-framework';
+
+const { height, width } = Dimensions.get('window');
+const orderConfirmedImage = require('../../assets/order_confirmed.png');
+
+class OrderConfirmed extends React.Component {
+  render() {
+    return (
+      <Block safe flex>
+        <NavBar
+          title="Confirmed Order"
+          onLeftPress={() => props.navigation.openDrawer()}
+          style={Platform.OS === 'android' ? { marginTop: theme.SIZES.BASE } : null}
+        />
+        <Block flex center space="around" style={styles.container}>
+          <Block center flex={2}>
+            <Block center style={{ marginBottom: theme.SIZES.BASE * 2 }}>
+              <Image
+                source={orderConfirmedImage}
+                style={{ marginBottom: theme.SIZES.BASE * 2 }}
+              />
+              <Text h4 color={theme.COLORS.BLACK}>
+                Well done!
+              </Text>
+            </Block>
+            <Text
+              color={theme.COLORS.BLACK}
+              style={{ marginBottom: theme.SIZES.BASE }}
+            >
+              <Text
+                size={theme.SIZES.FONT * 1.675}
+                bold
+              >
+                #45C23B&nbsp;
+              </Text>
+              <Text >
+                is your order number
+              </Text>
+            </Text>
+            <Text color={theme.COLORS.INFO}>
+              Track your order
+            </Text>
+          </Block>
+          <Button size="large" color="info" round onPress={() => {}}>
+            Continue Shopping
+          </Button>
+        </Block>
+      </Block>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: theme.SIZES.BASE * 0.3,
+    paddingHorizontal: theme.SIZES.BASE,
+    backgroundColor: theme.COLORS.WHITE,
+    marginTop: theme.SIZES.BASE * 1.875,
+    marginBottom: height * 0.1
+  }
+});
+
+export default OrderConfirmed;
