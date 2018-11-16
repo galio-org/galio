b522a444067c73c738d10bd2e6d12dad3061d865
Brais Antela <braantela@gmail.com>
2018-11-16 14:30:44 +0000

    Added: Switch component.

---
+++ src/Switch.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Switch as Switcher } from 'react-native';
+import PropTypes from 'prop-types';
+import GalioTheme, { withGalio } from './theme';
+
+class Switch extends Component {
+  state = {
+    switchValue: false,
+  };
+
+  onPressSwitch() {
+    this.setState({
+      switchValue: !this.state.switchValue,
+    });
+  }
+
+  render() {
+    const { color, disabled, trackColor, ios_backgroundColor, ...rest } = this.props;
+
+    color === 'primary' ? (trackColor.true = GalioTheme.COLORS.PRIMARY) : (trackColor.true = color);
+
+    return (
+      <Switcher
+        disabled={disabled}
+        trackColor={{ false: trackColor.false, true: trackColor.true }}
+        ios_backgroundColor={ios_backgroundColor}
+        value={this.state.switchValue}
+        onValueChange={() => {
+          this.onPressSwitch();
+        }}
+        {...rest}
+      />
+    );
+  }
+}
+
+Switch.defaultProps = {
+  color: GalioTheme.COLORS.PRIMARY,
+  ios_backgroundColor: GalioTheme.COLORS.GREY,
+  trackColor: {
+    false: GalioTheme.COLORS.GREY,
+    true: GalioTheme.COLORS.PRIMARY,
+  },
+  disabled: false,
+};
+
+Switch.propTypes = {
+  ...Switcher.propTypes,
+  color: PropTypes.oneOfType([PropTypes.oneOf(['primary']), PropTypes.string]),
+  disabled: PropTypes.bool,
+};
+
+export default withGalio(Switch);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Icon from './Icon';
 import Input from './Input';
 import NavBar from './NavBar';
 import Text from './Text';
+import Switch from './Switch';
 import theme, { withGalio, GalioProvider } from './theme';
 
-export { Block, Button, Card, Icon, Input, NavBar, Text, theme, withGalio, GalioProvider };
+export { Block, Button, Card, Icon, Input, NavBar, Text, Switch, theme, withGalio, GalioProvider };
