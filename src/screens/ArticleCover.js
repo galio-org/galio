7cae7c464b085620a8f43b2e762d6964d2e264d7
shubhamkakkar <shubham.kakkar98@gmail.com>
2019-07-15 02:12:39 +0530

    #102 resolved

--- src/screens/ArticleCover.js
+++ src/screens/ArticleCover.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import {
-  Image,
-  StyleSheet,
-  StatusBar,
-} from 'react-native';
+import { Image, StyleSheet, StatusBar } from 'react-native';
 import { Constants, LinearGradient } from 'expo';
 
-
 // galio components
-import {
-  Block, Icon, Text, NavBar,
-} from 'galio-framework';
+import { Block, Icon, Text, NavBar } from 'galio-framework';
 
 import theme from '../theme';
 
@@ -19,32 +12,49 @@ const ArticleCover = props => (
     <StatusBar hidden={false} barStyle="light-content" />
     <Image
       style={styles.backgroundImage}
-      source={{ uri: 'https://images.unsplash.com/photo-1537005081207-04f90e3ba640?fit=crop&w=764&q=80' }}
+      source={{
+        uri:
+          'https://images.unsplash.com/photo-1537005081207-04f90e3ba640?fit=crop&w=764&q=80',
+      }}
     />
     <Block flex space="between" center style={styles.absolute}>
-      <NavBar transparent leftIconColor={theme.COLORS.WHITE} onLeftPress={() => props.navigation.openDrawer()} />
+      <NavBar
+        transparent
+        leftIconColor={theme.COLORS.WHITE}
+        onLeftPress={() => props.navigation.openDrawer()}
+      />
       <Block style={styles.articleSummary}>
         <Block row style={{ marginBottom: theme.SIZES.BASE }}>
           <Block row middle style={{ marginRight: theme.SIZES.BASE }}>
-            <Icon name="eye" family="font-awesome" color={theme.COLORS.WHITE} size={theme.SIZES.FONT * 0.875} />
+            <Icon
+              name="eye"
+              family="font-awesome"
+              color={theme.COLORS.WHITE}
+              size={theme.SIZES.FONT * 0.875}
+            />
             <Text
               p
               color={theme.COLORS.WHITE}
               size={theme.SIZES.FONT * 0.875}
               style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
             >
-            25.6k
+              25.6k
             </Text>
           </Block>
           <Block row middle>
-            <Icon name="heart" family="font-awesome" color={theme.COLORS.WHITE} size={theme.SIZES.FONT * 0.875} />
+            <Icon
+              name="heart"
+              family="font-awesome"
+              color={theme.COLORS.WHITE}
+              size={theme.SIZES.FONT * 0.875}
+            />
             <Text
               p
               color={theme.COLORS.WHITE}
               size={theme.SIZES.FONT * 0.875}
               style={{ marginLeft: theme.SIZES.BASE * 0.25 }}
             >
-            936
+              936
             </Text>
           </Block>
         </Block>
@@ -60,20 +70,31 @@ const ArticleCover = props => (
           <Text
             size={theme.SIZES.FONT * 0.875}
             color={theme.COLORS.WHITE}
-            style={{ marginBottom: theme.SIZES.BASE, fontWeight: '500', lineHeight: theme.SIZES.FONT * 1.3 }}
+            style={{
+              marginBottom: theme.SIZES.BASE,
+              fontWeight: '500',
+              lineHeight: theme.SIZES.FONT * 1.3,
+            }}
           >
             Just small talk from the Fantastic Four.
           </Text>
           <Text
             size={theme.SIZES.FONT * 0.875}
             color={theme.COLORS.WHITE}
-            style={{ marginBottom: theme.SIZES.BASE / 2, fontWeight: '500', lineHeight: theme.SIZES.FONT * 1.3 }}
+            style={{
+              marginBottom: theme.SIZES.BASE / 2,
+              fontWeight: '500',
+              lineHeight: theme.SIZES.FONT * 1.3,
+            }}
           >
             So... Did you ever think about this bus? Like... How could this bus
             have all these weird colors. This purple is really cute though.
           </Text>
         </Block>
-        <LinearGradient colors={['transparent', 'rgba(0, 0, 0, 0.7)']} style={styles.gradient} />
+        <LinearGradient
+          colors={['transparent', 'rgba(0, 0, 0, 0.7)']}
+          style={styles.gradient}
+        />
       </Block>
     </Block>
   </Block>
