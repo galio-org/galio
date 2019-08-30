fb6ab6cb2733d31420de497ce160f2f3d8d9bc73
Shubham kakkar <shubham.kakkar98@gmail.com>
2019-08-30 15:22:51 +0530

    Update Icon.js

--- src/Icon.js
+++ src/Icon.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import GalioTheme, { withGalio } from './theme';
 import getIconType from './helpers/getIconType';
-import galioConfig from './fonts/galio.json';
+import galioConfig from './config/galio.json';
 
 const Galio = createIconSetFromIcoMoon(galioConfig, 'Galio', './fonts/galio.ttf');
 
