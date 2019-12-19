a1a4ddb816eb47cccff9e09072572c86937f20fe
Wiloke <hpvanlong@gmail.com>
2019-12-19 10:32:35 +0700

    add type GalioProvider + withGalio

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, ComponentType } from 'react';
 import {
   TouchableOpacityProps,
   ImageStyle,
@@ -233,4 +233,85 @@ declare module 'galio-framework' {
     onChange?: () => void;
   }
   export class Switch extends React.Component<SwitchProps> {}
+
+  export interface GalioProviderProps extends BaseProps {
+    theme: {
+      SIZES?: {
+        BASE?: number;
+        FONT?: number;
+        ICON?: number;
+        OPACITY?: number;
+        BORDER_RADIUS?: number;
+        BORDER_WIDTH?: number;
+        BUTTON_WIDTH?: number;
+        BUTTON_HEIGHT?: number;
+        BUTTON_SHADOW_RADIUS?: number;
+        BLOCK_SHADOW_OPACITY?: number;
+        BLOCK_SHADOW_RADIUS?: number;
+        ANDROID_ELEVATION?: number;
+        CARD_BORDER_RADIUS?: number;
+        CARD_BORDER_WIDTH?: number;
+        CARD_WIDTH?: number;
+        CARD_MARGIN_VERTICAL?: number;
+        CARD_FOOTER_HORIZONTAL?: number;
+        CARD_FOOTER_VERTICAL?: number;
+        CARD_AVATAR_WIDTH?: number;
+        CARD_AVATAR_HEIGHT?: number;
+        CARD_AVATAR_RADIUS?: number;
+        CARD_IMAGE_HEIGHT?: number;
+        CARD_ROUND?: number;
+        CARD_ROUNDED?: number;
+        INPUT_BORDER_RADIUS?: number;
+        INPUT_BORDER_WIDTH?: number;
+        INPUT_HEIGHT?: number;
+        INPUT_HORIZONTAL?: number;
+        INPUT_TEXT?: number;
+        INPUT_LABEL_TEXT?: number;
+        INPUT_LABEL_BOTTOM?: number;
+        INPUT_HELP_TEXT?: number;
+        INPUT_ROUNDED?: number;
+        NAVBAR_HEIGHT?: number;
+        NAVBAR_VERTICAL?: number;
+        NAVBAR_TITLE_FLEX?: number;
+        NAVBAR_TITLE_HEIGHT?: number;
+        NAVBAR_TITLE_TEXT?: number;
+        NAVBAR_LEFT_FLEX?: number;
+        NAVBAR_LEFT_HEIGHT?: number;
+        NAVBAR_LEFT_MARGIN?: number;
+        NAVBAR_RIGHT_FLEX?: number;
+        NAVBAR_RIGHT_HEIGHT?: number;
+        NAVBAR_RIGHT_MARGIN?: number;
+        [key: string]: number;
+      };
+      COLORS?: {
+        FACEBOOK?: string;
+        TWITTER?: string;
+        DRIBBBLE?: string;
+        THEME?: string;
+        PRIMARY?: string;
+        INFO?: string;
+        ERROR?: string;
+        WARNING?: string;
+        INPUT?: string;
+        PLACEHOLDER?: string;
+        NAVBAR?: string;
+        BLOCK?: string;
+        ICON?: string;
+        WHITE?: string;
+        BLACK?: string;
+        GREY?: string;
+        MUTED?: string;
+        TRANSPARENT?: string;
+        NEUTRAL?: string;
+        [key: string]: string;
+      };
+    };
+  }
+  export class GalioProvider extends React.Component<GalioProviderProps> {}
+
+  type NamedStyles = ViewStyle | TextStyle | ImageStyle;
+  export function withGalio<T extends ComponentType<any>>(
+    Component: T,
+    styles: NamedStyles
+  ): ComponentType<any>;
 }
