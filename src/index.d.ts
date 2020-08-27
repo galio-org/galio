b813cac80b74eb7563af7b6dd4e0b60b919ed3f3
Rommel Rico <me@rommelrico.com>
2020-08-26 21:20:33 -0700

    Checking the component props

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,327 +1,314 @@
-import React, { ReactNode, ComponentType } from 'react';
+import React, { ReactNode } from 'react';
 import {
   TouchableOpacityProps,
-  ImageStyle,
   ViewStyle,
-  TextStyle,
-  SwitchProps as RNSwitchProps,
-  TextInputProps,
+  SwitchProps as RNSwitchProps
 } from 'react-native';
 
 declare module 'galio-framework' {
-  type IconFamilyType =
-    | 'Galio'
-    | 'AntDesign'
-    | 'Entypo'
-    | 'EvilIcons'
-    | 'Feather'
-    | 'FontAwesome'
-    | 'FontAwesome5'
-    | 'Fontisto'
-    | 'Foundation'
-    | 'Ionicons'
-    | 'MaterialIcons'
-    | 'MaterialCommunityIcons'
-    | 'Octicons'
-    | 'Zocial'
-    | 'SimpleLineIcons';
-
-  type BaseColorType = string;
-
-  type ButtonColorType = string;
 
   interface BaseProps {
     [key: string]: any;
   }
 
-  export interface BlockProps extends BaseProps {
-    bottom?: boolean;
-    card?: boolean;
-    center?: boolean;
-    flex?: boolean | number;
-    fluid?: boolean;
-    height?: number;
-    left?: boolean;
-    middle?: boolean;
-    right?: boolean;
-    row?: boolean;
-    safe?: boolean;
-    shadow?: boolean;
-    shadowColor?: boolean;
-    space?: string;
-    top?: boolean;
-    width?: number;
-  }
-  export class Block extends React.Component<BlockProps> {}
-
+  // Button
   export interface ButtonProps extends TouchableOpacityProps, BaseProps {
-    capitalize?: boolean;
-    color?: ButtonColorType;
+    color?:
+      | ['theme', 'primary', 'info', 'danger', 'warning', 'success', 'black', 'grey', 'secondary', 'transparent', 'white']
+      | string;
+    size?:
+      | ['large' | 'default' | 'small']
+      | number;
+    iconColor?: string;
     disabled?: boolean;
-    icon?: string;
-    iconColor?: boolean | string;
+    uppercase?: boolean;
+    lowercase?: boolean;
+    capitalize?: boolean;
+    loading?: boolean;
+    loadingSize?: 'small' | 'default' | 'large';
+    opacity?: number;
+    shadowless?: boolean;
+    shadowColor?: boolean | string;
+    onlyIcon?: boolean;
+    icon?: boolean | string;
+    iconRight?: string | boolean;
     iconFamily?: boolean | string;
     iconSize?: number;
-    loading?: boolean;
-    loadingSize?: 'small' | 'large';
-    lowercase?: boolean;
-    onlyIcon?: boolean;
-    opacity?: number;
-    radius?: number;
-    shadowColor?: boolean | string;
-    shadowless?: boolean;
-    size?: 'small' | 'large' | number;
-    uppercase?: boolean;
+    styles?: any;
+    theme?: any;
   }
   export class Button extends React.Component<ButtonProps> {}
 
-  export interface CardProps extends BaseProps {
-    card?: boolean;
-    shadow?: boolean;
-    borderless?: boolean;
-    image?: string;
-    imageBlockStyle?: ViewStyle;
-    imageStyle?: ImageStyle;
-    avatar?: string;
-    location?: string;
-    locationColor?: boolean | string;
-    title?: string;
-    titleColor?: string;
-    caption?: string;
-    captionColor?: string;
-    footerStyle?: ViewStyle;
-  }
-  export class Card extends React.Component<CardProps> {}
-
-  export interface CheckBoxProps extends BaseProps {
-    checkboxStyle?: ViewStyle;
-    disabled?: boolean;
-    flexDirection?: ViewStyle['flexDirection'];
-    iconName?: string;
-    iconSize?: number;
-    iconFamily?: IconFamilyType;
-    image?: string;
-    imageStyle?: ImageStyle;
-    initialValue?: boolean;
-    label?: string;
-    labelStyle?: TextStyle;
-    onChange?: () => void;
-  }
-  export class CheckBox extends React.Component<CheckBoxProps> {}
-
-  export interface DeckSwiperProps extends BaseProps {
-    style?: ViewStyle;
-    components?: ReactNode[];
-    onSwipeRight?: () => void;
-    onSwipeLeft?: () => void;
-    focusedElementStyle?: ViewStyle;
-    nextElementStyle?: ViewStyle;
-  }
-  export class DeckSwiper extends React.Component<DeckSwiperProps> {}
-
-  export interface IconProps extends BaseProps {
-    name?: string;
-    family?: IconFamilyType;
-    size?: number;
-    color?: string;
-  }
-  export class Icon extends React.Component<IconProps> {}
-
-  export interface InputProps
-    extends Omit<
-      TextInputProps,
-      | 'style'
-      | 'keyboardType'
-      | 'secureTextEntry'
-      | 'placeholderTextColor'
-      | 'underlineColorAndroid'
-      >,
-      BaseProps {
-    type?: TextInputProps['keyboardType'];
+  // Input
+  export interface InputProps extends BaseProps {
+    style?: any;
+    textInputStyle?: any;
+    type?: string;
     password?: boolean;
+    placeholderTextColor?: string;
     label?: string;
     bgColor?: string;
     rounded?: boolean;
     borderless?: boolean;
     viewPass?: boolean;
-    icon?: string;
     iconColor?: string;
-    iconContent?: any;
-    family?: IconFamilyType;
+    icon?: string;
+    family?: string;
     color?: string;
     help?: string;
     left?: boolean;
     right?: boolean;
     topHelp?: boolean;
     bottomHelp?: boolean;
+    styles?: any;
     iconSize?: number;
+    iconContent?: any;
+    theme?: any;
+    onRef?: () => void;
   }
   export class Input extends React.Component<InputProps> {}
 
-  export interface NavBarProps extends BaseProps {
-    back?: boolean;
-    transparent?: boolean;
-    title?: ReactNode;
-    titleStyle?: ViewStyle;
-    left?: ReactNode;
-    leftStyle?: ViewStyle;
-    leftIconColor?: string;
-    right?: ReactNode;
-    rightStyle?: ViewStyle;
-    onLeftPress?: () => void;
+  // Link
+  export interface LinkProps extends BaseProps {
+    children?: any;
+    theme?: any;
+    onPress: () => void;
   }
-  export class NavBar extends React.Component<NavBarProps> {}
+  export class Link extends React.Component<LinkProps> {}
 
-  export interface RadioProps extends BaseProps {
+  // Icon
+  export interface IconProps extends BaseProps {
+    name: string;
+    family: string;
+    size?: number;
     color?: string;
-    containerStyle?: ViewStyle;
-    radioOuterStyle?: ViewStyle;
-    radioInnerStyle?: ViewStyle;
-    flexDirection?: ViewStyle['flexDirection'];
-    initialValue?: boolean;
-    label?: string;
-    labelStyle?: TextStyle;
-    onChange?: () => void;
+    styles?: any;
+    theme?: any;
   }
-  export class Radio extends React.Component<RadioProps> {}
+  export class Icon extends React.Component<IconProps> {}
 
+  // Text
   export interface TextProps extends BaseProps {
+    children?: any;
+    style?: any;
     h1?: boolean;
     h2?: boolean;
     h3?: boolean;
     h4?: boolean;
     h5?: boolean;
+    h6?: boolean;
     p?: boolean;
+    body?: boolean;
+    small?: boolean;
     size?: number;
     color?: string;
     muted?: boolean;
     bold?: boolean;
     italic?: boolean;
+    styles?: any;
+    theme?: any;
   }
   export class Text extends React.Component<TextProps> {}
 
-  export interface ToastProps extends BaseProps {
-    style?: ViewStyle;
-    children?: ReactNode;
-    isShow?: boolean;
-    positionIndicator?: 'top' | 'center' | 'bottom';
-    positionOffset?: number;
-    fadeInDuration?: number;
-    fadeOutDuration?: number;
-    color?: BaseColorType;
-    round?: boolean;
-    textStyle?: TextStyle;
+  // GalioProvider
+  export interface GalioProviderProps extends BaseProps {
+    children?: any;
+    theme?: any;
   }
-  export class Toast extends React.Component<ToastProps> {}
+  export class GalioProvider extends React.Component<GalioProviderProps> {}
 
+  // Accordion
+  export interface AccordionProps extends BaseProps {
+    theme?: any;
+    dataArray?: [];
+    opened?: number;
+    listStyle?: any;
+    style?: any;
+    icon?: any;
+    expandedIcon?: any;
+    headerStyle?: any;
+    contentStyle?: any;
+    onAccordionClose?: () => void;
+    onAccordionOpen?: () => void;
+  }
+  export class Accordion extends React.Component<AccordionProps> {}
+
+  // Avatar
+  export interface AvatarProps extends BaseProps {
+    label?: string;
+    labelColor?: string;
+    size?: number;
+    source?: object | number;
+    backgroundColor?: string;
+    imageProps?: object;
+    imageStyle?: object | [] | number;
+    containerStyle?: ViewStyle;
+    styles?: any;
+    theme?: any;
+  }
+  export class Avatar extends React.Component<AvatarProps> {}
+
+  // Block
+  export interface BlockProps extends BaseProps {
+    row?: boolean;
+    flex?: boolean | number;
+    center?: boolean;
+    middle?: boolean;
+    top?: boolean;
+    bottom?: boolean;
+    right?: boolean;
+    card?: boolean;
+    left?: boolean;
+    shadow?: boolean;
+    space?: string;
+    fluid?: boolean;
+    height?: number;
+    width?: number;
+    shadowColor?: string;
+    safe?: boolean;
+    styles?: any;
+    theme?: any;
+  }
+  export class Block extends React.Component<BlockProps> {}
+
+  // Card
+  export interface CardProps extends BaseProps {
+    card?: boolean;
+    shadow?: boolean;
+    borderless?: boolean;
+    styles?: any;
+    theme?: any;
+    title?: string;
+    titleColor?: string;
+    caption?: string;
+    captionColor?: string;
+    avatar?: string;
+    footerStyle?: object;
+  }
+  export class Card extends React.Component<CardProps> {}
+
+  // Checkbox
+  export interface CheckboxProps extends BaseProps {
+    checkboxStyle?: any;
+    color?:
+      | ['primary' | 'theme' | 'error' | 'warning' | 'success' | 'transparent' | 'info']
+      | string;
+    disabled?: boolean;
+    flexDirection?:
+      | ['row' | 'row-reverse' | 'column' | 'column-reverse']
+      | string;
+    iconColor?: string;
+    iconName?: string;
+    iconSize?: number;
+    iconFamily?: string;
+    image?: string;
+    imageStyle?: any;
+    initialValue?: boolean;
+    label: string;
+    labelStyle?: any;
+    onChange?: () => void;
+    styles?: any;
+    theme?: any;
+  }
+  export class Checkbox extends React.Component<CheckboxProps> {}
+
+  // DeckSwiper
+  export interface DeckSwiperProps extends BaseProps {
+    components: [];
+    onSwipeRight?: () => void;
+    onSwipeLeft?: () => void;
+    focusedElementStyle?: any;
+    nextElementStyle?: any;
+    style?: any;
+  }
+  export class DeckSwiper extends React.Component<DeckSwiperProps> {}
+
+  // NavBar
+  export interface NavBarProps extends BaseProps {
+    back?: boolean;
+    transparent?: boolean;
+    title?: ReactNode | string;
+    titleStyle?: any;
+    left?: ReactNode;
+    leftStyle?: any;
+    leftIconColor?: string;
+    onLeftPress?: () => void;
+    leftHitSlop?: any;
+    right?: ReactNode;
+    rightStyle?: any;
+    style?: any;
+    styles?: any;
+    theme?: any;
+    leftIconName?: string;
+    leftIconFamily?: string;
+    hideLeft?: boolean;
+    hideRight?: boolean;
+  }
+  export class NavBar extends React.Component<NavBarProps> {}
+
+  // Radio
+  export interface RadioProps extends BaseProps {
+    color?: string;
+    containerStyle?: any;
+    radioOuterStyle?: any;
+    radioInnerStyle?: any;
+    disabled?: boolean;
+    flexDirection?:
+      | ['row' | 'row-reverse' | 'column' | 'column-reverse']
+      | string;
+    initialValue?: boolean;
+    label: string;
+    labelStyle?: any;
+    onChange?: () => void;
+    styles?: any;
+    theme?: any;
+  }
+  export class Radio extends React.Component<RadioProps> {}
+
+  // Slider
   export interface SliderProps extends BaseProps {
-    activeColor?: string;
     value?: number;
     disabled?: boolean;
     minimumValue?: number;
     maximumValue?: number;
-    trackStyle?: ViewStyle;
-    thumbStyle?: ViewStyle;
+    trackStyle?: any;
+    thumbStyle?: any;
     step?: number;
+    styles?: any;
     onSlidingComplete?: () => void;
     onSlidingStart?: () => void;
     onValueChange?: () => void;
   }
   export class Slider extends React.Component<SliderProps> {}
 
+  // Switch
   export interface SwitchProps extends RNSwitchProps, BaseProps {
-    color?: BaseColorType;
+    color?:
+      | ['primary' | 'theme' | 'error' | 'warning' | 'success' | 'info']
+      | string;
     disabled?: boolean;
     initialValue?: boolean;
-    trackColor?: {
-      false: string;
-      true: string;
-    };
-    ios_backgroundColor?: string;
-    onChange?: () => void;
+    onChange: () => void;
   }
   export class Switch extends React.Component<SwitchProps> {}
 
-  interface ThemeType {
-    SIZES?: {
-      BASE?: number;
-      FONT?: number;
-      ICON?: number;
-      OPACITY?: number;
-      BORDER_RADIUS?: number;
-      BORDER_WIDTH?: number;
-      BUTTON_WIDTH?: number;
-      BUTTON_HEIGHT?: number;
-      BUTTON_SHADOW_RADIUS?: number;
-      BLOCK_SHADOW_OPACITY?: number;
-      BLOCK_SHADOW_RADIUS?: number;
-      ANDROID_ELEVATION?: number;
-      CARD_BORDER_RADIUS?: number;
-      CARD_BORDER_WIDTH?: number;
-      CARD_WIDTH?: number;
-      CARD_MARGIN_VERTICAL?: number;
-      CARD_FOOTER_HORIZONTAL?: number;
-      CARD_FOOTER_VERTICAL?: number;
-      CARD_AVATAR_WIDTH?: number;
-      CARD_AVATAR_HEIGHT?: number;
-      CARD_AVATAR_RADIUS?: number;
-      CARD_IMAGE_HEIGHT?: number;
-      CARD_ROUND?: number;
-      CARD_ROUNDED?: number;
-      INPUT_BORDER_RADIUS?: number;
-      INPUT_BORDER_WIDTH?: number;
-      INPUT_HEIGHT?: number;
-      INPUT_HORIZONTAL?: number;
-      INPUT_TEXT?: number;
-      INPUT_LABEL_TEXT?: number;
-      INPUT_LABEL_BOTTOM?: number;
-      INPUT_HELP_TEXT?: number;
-      INPUT_ROUNDED?: number;
-      NAVBAR_HEIGHT?: number;
-      NAVBAR_VERTICAL?: number;
-      NAVBAR_TITLE_FLEX?: number;
-      NAVBAR_TITLE_HEIGHT?: number;
-      NAVBAR_TITLE_TEXT?: number;
-      NAVBAR_LEFT_FLEX?: number;
-      NAVBAR_LEFT_HEIGHT?: number;
-      NAVBAR_LEFT_MARGIN?: number;
-      NAVBAR_RIGHT_FLEX?: number;
-      NAVBAR_RIGHT_HEIGHT?: number;
-      NAVBAR_RIGHT_MARGIN?: number;
-      [key: string]: number;
-    };
-    COLORS?: {
-      FACEBOOK?: string;
-      TWITTER?: string;
-      DRIBBBLE?: string;
-      THEME?: string;
-      PRIMARY?: string;
-      INFO?: string;
-      ERROR?: string;
-      WARNING?: string;
-      INPUT?: string;
-      PLACEHOLDER?: string;
-      NAVBAR?: string;
-      BLOCK?: string;
-      ICON?: string;
-      WHITE?: string;
-      BLACK?: string;
-      GREY?: string;
-      MUTED?: string;
-      TRANSPARENT?: string;
-      NEUTRAL?: string;
-      [key: string]: string;
-    };
+  // Toast
+  export interface ToastProps extends BaseProps {
+    children: ReactNode;
+    isShow: boolean;
+    positionIndicator: 'top' | 'center' | 'bottom';
+    positionOffset?: number;
+    fadeInDuration?: number;
+    fadeOutDuration?: number;
+    color?:
+      | ['primary', 'theme', 'info', 'error', 'warning', 'success']
+      | string;
+    round?: boolean;
+    style?: ViewStyle;
+    textStyle?: ViewStyle;
+    styles?: any;
+    theme?: any;
   }
-  export const theme: ThemeType = {};
-  export interface GalioProviderProps extends BaseProps {
-    theme: ThemeType;
-  }
-  export class GalioProvider extends React.Component<GalioProviderProps> {}
+  export class Toast extends React.Component<ToastProps> {}
 
-  type NamedStyles = ViewStyle | TextStyle | ImageStyle;
-  export function withGalio<T extends ComponentType<any>>(
-    Component: T,
-    styles: NamedStyles
-  ): ComponentType<any>;
 }
