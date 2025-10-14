93cb3a7be90cc22211630815cc7187b40ab2ef3a
Beniamin Marcu <beniamin.marcu93@gmail.com>
2025-10-14 16:35:33 +0300

    Remove atomic UI components and theme helpers
    Deleted all files related to atomic/atoms (Button, Input, Link), atomic/ions (icon, text), helpers (getIconType, normalize), and theme (colors, index, sizes). This likely indicates a refactor, migration, or removal of unused UI components and related utilities.

--- helpers/getIconType.d.ts
+++
@@ -1,3 +0,0 @@
-export type IconFamily = 'antdesign' | 'entypo' | 'evilicons' | 'feather' | 'fontawesome' | 'fontawesome5' | 'foundation' | 'ionicons' | 'materialcommunityicons' | 'materialicons' | 'octicons' | 'simplelineicons' | 'zocial';
-export default function getIconType(type: string): any;
-//# sourceMappingURL=getIconType.d.ts.map
